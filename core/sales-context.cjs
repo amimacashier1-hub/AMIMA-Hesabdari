ed9efc416bfe56988fe49962f87702f6f93f4289
amimacashier1-hub <  GitHub>
2026-09-14 18:19:43 +0330

    Add sales core context

---
+++ core/sales-context.cjs
@@ -0,0 +1,61 @@
+'use strict';
+
+function createSalesContext(deps) {
+  const required = [
+    'db',
+    'rows',
+    'money',
+    'newId',
+    'withTransaction',
+    'invoiceDetail',
+    'nextInvoiceNo',
+    'ledgerStock',
+    'movingAverageCost',
+    'movementCost',
+    'syncProductStockFromLedger',
+    'tierFor',
+    'repriceInvoiceProduct',
+    'recalcInvoice',
+    'queueSync',
+    'auditLog',
+    'postJournal',
+    'postJournalOnce',
+    'insertCashMovement'
+  ];
+
+  for (const name of required) {
+    if (typeof deps[name] === 'undefined') {
+      throw new Error(`Sales Context missing dependency: ${name}`);
+    }
+  }
+
+  return Object.freeze({
+    db: deps.db,
+    rows: deps.rows,
+    money: deps.money,
+    newId: deps.newId,
+    withTransaction: deps.withTransaction,
+
+    invoiceDetail: deps.invoiceDetail,
+    nextInvoiceNo: deps.nextInvoiceNo,
+
+    ledgerStock: deps.ledgerStock,
+    movingAverageCost: deps.movingAverageCost,
+    movementCost: deps.movementCost,
+    syncProductStockFromLedger: deps.syncProductStockFromLedger,
+
+    tierFor: deps.tierFor,
+    repriceInvoiceProduct: deps.repriceInvoiceProduct,
+    recalcInvoice: deps.recalcInvoice,
+
+    queueSync: deps.queueSync,
+    auditLog: deps.auditLog,
+
+    postJournal: deps.postJournal,
+    postJournalOnce: deps.postJournalOnce,
+
+    insertCashMovement: deps.insertCashMovement
+  });
+}
+
+module.exports = { createSalesContext };
